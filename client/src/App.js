9493818128ca67034cce7b40b202f82abdd8d709
Smruti Ranjan Badatya <smrutiranjanbadatya2@gmail.com>
2023-04-18 00:15:36 +0530

    created file - README

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <>Hello WOrld</>
+    <Router>
+      <Route>
+        <Route path='/' element={<>Home</>}/>
+        
+      </Route>
+    </Router>
   );
 }
 
