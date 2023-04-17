4a5a272fb680a6a7f6e6b99db1898c7924f7db99
Smruti Ranjan Badatya <smrutiranjanbadatya2@gmail.com>
2023-04-18 00:24:14 +0530

    added dependencies and routes to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login';
+import ProtectedRoute from './components/ProtectedRoute';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <Router>
-      <Route>
-        <Route path='/' element={<>Home</>}/>
-        
-      </Route>
+      <Routes>
+        <Route path='/login' element={<Login />}/>
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
     </Router>
   );
 }
