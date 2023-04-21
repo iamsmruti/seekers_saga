8ce68ce907172f2276ac86c45b735574b1d86557
Smruti Ranjan Badatya <smrutiranjanbadatya2@gmail.com>
2023-04-22 02:40:46 +0530

    worked on admin

--- admin/src/pages/Hunts/index.js
+++ admin/src/pages/Hunts/index.js
@@ -17,7 +17,7 @@ const Hunts = () => {
       <Heading title={"Hunts"} subtitle={"Manage and create Treasure Hunts"} action={<AddButton onClick={handleClick} />}/>
 
       <div className='grid grid-cols-1 md:grid-cols-3 gap-6 m-2'>
-        <RiddleCard title={"Find the odd one..."} time={"2min"} level={"intermediate"} image={"https://images.squarespace-cdn.com/content/v1/532b3fbfe4b0c5a9ade0bc44/1397499480045-6LXKJ4CAJOX4O5LK9I2A/Treasurehuntgame1.jpg?format=1000w"} path={"/hunts/1"}/>
+        <RiddleCard title={"Treasure Hunt - Chapter 1"} time={"2min"} level={"intermediate"} image={"https://images.squarespace-cdn.com/content/v1/532b3fbfe4b0c5a9ade0bc44/1397499480045-6LXKJ4CAJOX4O5LK9I2A/Treasurehuntgame1.jpg?format=1000w"} path={"/hunts/1"}/>
       </div>
     </Layout>
   )
