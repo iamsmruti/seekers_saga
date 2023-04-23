45416a0bb95e460933cd0c6d3579c961113993ef
Smruti Ranjan Badatya <smrutiranjanbadatya2@gmail.com>
2023-04-24 01:33:24 +0530

    changed to prod url

--- admin/src/constants.js
+++ admin/src/constants.js
@@ -1,2 +1,2 @@
-export const API = 'http://localhost:4343' 
-// export const API = 'https://seekerssaga-production.up.railway.app' 
+// export const API = 'http://localhost:4343' 
+export const API = 'https://seekerssaga-production.up.railway.app' 
