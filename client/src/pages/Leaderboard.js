b94bb7798c9c17b08f8ecb2d6a03a3550e97b376
Smruti Ranjan Badatya <smrutiranjanbadatya2@gmail.com>
2023-04-25 20:56:52 +0530

    fixed attribute issue in leaderboard

--- client/src/pages/Leaderboard.js
+++ client/src/pages/Leaderboard.js
@@ -39,7 +39,7 @@ const Leaderboard = () => {
                         <div className='col-span-2 md:text-left text-center'>{record.score}</div>
                     </div> : <div className='grid grid-cols-12 bg-blue-100 py-2 pl-2'>
                     <div className='col-span-1'># {index + 1}</div>
-                        <div className='col-span-5'>{record.user}</div>
+                        <div className='col-span-5'>{record.userID}</div>
                         <div className='col-span-2 md:text-left text-center'>{record.totalTime}</div>
                         <div className='col-span-2 md:text-left text-center'>{record.totalAttempts}</div>
                         <div className='col-span-2 md:text-left text-center'>{record.score}</div>
