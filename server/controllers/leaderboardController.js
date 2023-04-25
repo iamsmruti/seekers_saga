9263c90351a574ef752b463cc78dfbd6fd3b630a
Smruti Ranjan Badatya <smrutiranjanbadatya2@gmail.com>
2023-04-25 21:01:50 +0530

    changed leaderboard controller to display desc

--- server/controllers/leaderboardController.js
+++ server/controllers/leaderboardController.js
@@ -5,7 +5,7 @@ export const getAll = async (req, res) => {
     const id = req.params.id
     
     try {
-        const records = await LeaderBoard.find({gameID: id})
+        const records = await LeaderBoard.find({gameID: id}).sort({score: -1})
 
         res.status(200).json(records)
     } catch(err) {
